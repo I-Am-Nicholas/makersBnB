39c16c263ba35cac7d2ca8b31e8bca59a1acaeca
Albert <ayanit1@googlemail.com>
2017-03-21 12:37:59 +0000

    Refactoring previous commits

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -7,11 +7,7 @@ feature 'User Sign-Up' do
 
   scenario 'welcomes user after signing up' do
     visit '/users/new'
-    fill_in :name, with: 'Albert'
-    fill_in :password, with: 'password'
-    fill_in :confirm_password, with: 'password'
-    fill_in :email, with: 'albert@albert.com'
-    click_button 'Submit'
+    sign_up
     expect(page).to have_content('Welcome Albert!')
   end
 end
