3b2065681fa6ac13a17686081d299d88cb5349e0
Nicholas Leacock <nicholas@swissmail.com>
2017-03-21 10:40:10 +0000

    Added BCrypt functionality to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,22 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require 'bcrypt'
 
 
 class User
   include DataMapper::Resource
+  include BCrypt
+
 
   property :id, Serial
   property :name, String
-  property :password, Text
+  property :password_digest, Text
   property :email, String
 
   has n, :places
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
 end
