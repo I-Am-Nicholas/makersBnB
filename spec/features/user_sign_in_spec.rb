3a8b7e66b46f0bb3ec7c27e19a96f635eb306611
Albert <ayanit1@googlemail.com>
2017-03-22 18:41:51 +0000

    Added to integration folder

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -7,17 +7,4 @@ feature 'User Sign in' do
     expect(page).to have_content "Welcome Albert!"
   end
 
-  feature 'Edge casing' do
-
-    scenario 'throws an error when an empty field is present' do
-      sign_up_with_a_blank_name
-      expect(page).to have_content "Empty field: please check sign up form and try again"
-    end
-
-    scenario 'throws an error with incorrect credentials' do
-      sign_in_with_false_details
-      expect(page).to have_text "Incorrect email or password: Please try again"
-    end
-  end
-  
 end
---
+++ spec/intergrations/sign_up_edge_casing_spec.rb
@@ -0,0 +1,23 @@
+feature 'Sign Up Edge Casing' do
+
+  scenario 'throw error when user enters password shorter than 6 characters' do
+    visit '/users/new'
+    fill_in :name, with: 'Albert'
+    fill_in :password, with: 'passw'
+    fill_in :confirm_password, with: 'passw'
+    fill_in :email, with: 'albert@albert.com'
+    click_button 'Submit'
+    expect(page).to have_content("Password length error: Password must be longer than 6 characters")
+  end
+
+  scenario 'throw error when user does not enter an @ sign' do
+    visit '/users/new'
+    fill_in :name, with: 'Albert'
+    fill_in :password, with: 'password'
+    fill_in :confirm_password, with: 'password'
+    fill_in :email, with: 'albert.com'
+    click_button 'Submit'
+    expect(page).to have_content("E-mail character error: Missing @ sign")
+  end
+  
+end
