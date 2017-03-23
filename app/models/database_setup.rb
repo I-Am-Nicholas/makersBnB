dc75076f850f8d486561ebd66f43914572abd1b4
Nicholas Leacock <nicholas@swissmail.com>
2017-03-23 13:04:13 +0000

    Deployed to Heroku

--- app/models/database_setup.rb
+++ app/models/database_setup.rb
@@ -3,6 +3,6 @@ require 'dm-postgres-adapter'
 require './app/models/place'
 require './app/models/user'
 
-DataMapper.setup(:default, "postgres://localhost/bnb_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
