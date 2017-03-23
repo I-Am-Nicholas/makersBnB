6fa11a96a116d7ce39c9b1cd4a5cb25f5c6847ca
Ashwini <ashie.oh@gmail.com>
2017-03-23 17:50:29 +0000

    Adds a feature test for booking creation

--- spec/features/booking_spec.rb
+++ spec/features/booking_spec.rb
@@ -4,7 +4,11 @@ feature 'book new place for hire' do
     add_place
     click_button('bookthis')
     expect(page).to have_content('The Joyroom')
-    click_button('book')
+    fill_in :message, with: 'Let me book!'
+    fill_in :book_from, with: '2017-03-30'
+    fill_in :book_to, with: '2017-04-01'
+    expect{click_button('book')}.to change(Booking, :count).by(1)
     expect(page).to have_content("Hello")
   end
+  
 end
